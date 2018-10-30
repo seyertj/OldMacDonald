f9516fe745cadab7505435f565b8b68130a2b5bf
JesusThomas Reyes <JeReyes@sfusd.edu>
2018-10-30 09:59:19 -0700

    finished

--- Chick.java
+++ Chick.java
@@ -1,4 +1,30 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal 
+{
+  private String myType;
+  private String mySound;
+  public Chick(String type, String sound)
+  {
+    myType = type;
+    mySound = sound;
+  }
+  public Chick(String type, String sound, String sound2)
+  {
+    myType = "chick";
+    if((int)(Math.random()*2) == 0)
+    {
+      mySound = sound;
+    }
+    else
+    {
+      mySound = sound2;
+    }
+  }
+  public String getSound()
+  {
+    return mySound;
+  }
+  public String getType()
+  {
+    return myType;
+  }
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,22 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+  private String cowName;
+  private String nType;
+  private String nSound;
+  public NamedCow(String name, String type, String sound)
+  {
+    cowName = name;
+    nType = type;
+    nSound = sound;
+  }
+  public NamedCow()
+  {
+    cowName = "Cory";
+    nType = "cow";
+    nSound = "mooo";
+  }
+  public String getName()
+  {
+    return cowName;
+  }
 }
